e15be1d802b0da4e7892060a57a310c4f5cd4529
nothxmza <terras.hamza@hotmail.fr>
2025-02-26 13:39:53 +0100

    refacto

--- index.js
+++ index.js
@@ -1,76 +1,30 @@
 import { recipes } from "./data/recipes.js";
 import { listTemplates, cardTemplates, updateList } from "./scripts/templates/templates.js";
+import { listSearch } from "./scripts/utils/list.js";
 
 
-let currentRecipes = [...recipes];
-let tags = new Set();
+export let currentRecipes = [...recipes];
+export let tags = new Set();
+export let count = currentRecipes ? currentRecipes.length : 0;
 
-const toggleIngredient = () => {
-	const list = document.querySelector('.ingredient-list');
-	const toggleDevice = document.querySelector('.toggle-ingredient');
-	const btnListe = document.querySelector('.btn-ingredient-list');
-
-	list.classList.toggle('hidden');
-	toggleDevice.classList.toggle('hidden');
-
-	btnListe.addEventListener('click', () => {
-		list.classList.add('hidden');
-		toggleDevice.classList.remove('hidden');
-	})
-}
-window.toggleIngredient = toggleIngredient;
-
-const toggleDevices = () => {
-	const list = document.querySelector('.devices-list');
-	const toggleDevice = document.querySelector('.toggle-devices');
-	const btnListe = document.querySelector('.btn-devices-list');
-
-	list.classList.toggle('hidden');
-	toggleDevice.classList.toggle('hidden');
-
-	btnListe.addEventListener('click', () => {
-		list.classList.add('hidden');
-		toggleDevice.classList.remove('hidden');
-	})
-	
-}
-window.toggleDevices = toggleDevices;
-
-
-const toggleUstensils = () => {
-	const list = document.querySelector('.ustensils-list');
-	const toggleDevice = document.querySelector('.toggle-ustensils');
-	const btnListe = document.querySelector('.btn-ustensils-list');
-
-	list.classList.toggle('hidden');
-	toggleDevice.classList.toggle('hidden');
-
-	btnListe.addEventListener('click', () => {
-		list.classList.add('hidden');
-		toggleDevice.classList.remove('hidden');
-	})
-	
-}
-window.toggleUstensils = toggleUstensils;
 
 const mainSearch = () => {
 	const search = document.getElementById('main-search');
-
 	let searchValue = "";
 	let searchValueLower = "";
 
+	//sort recipes by search value
 	search.addEventListener('input', (e) => {
 		searchValue = e.target.value;
-
 		if(searchValue.length > 2){
 			searchValueLower = searchValue.toLowerCase();
 			currentRecipes = recipes.filter(recipe => {
 				const recipeName = recipe.name.toLowerCase();
 				const recipeDescription = recipe.description.toLowerCase();
-				const ingredientMatch = recipe.ingredients.filter(ingredient => {
+				const ingredientMatch = recipe.ingredients.some(ingredient => {
 					return ingredient.ingredient.toLowerCase().includes(searchValueLower);
 				})
-				return recipeDescription.includes(searchValueLower) || recipeName.includes(searchValueLower) || ingredientMatch;
+				return recipeName.includes(searchValueLower) || recipeDescription.includes(searchValueLower) || ingredientMatch;
 			})
 		}else{
 			currentRecipes = [...recipes];
@@ -79,23 +33,13 @@ const mainSearch = () => {
 	})
 }
 
-const updateWithTags = () => {
+export const updateWithTags = () => {
 	const searchValue = document.getElementById('main-search').value;
-	currentRecipes = [...recipes];
 
-	if (searchValue.length > 2) {
-		currentRecipes = currentRecipes.filter(recipe => {
-			const recipeName = recipe.name.toLowerCase();
-			const recipeDescription = recipe.description.toLowerCase();
-			const ingredientMatch = recipe.ingredients.some(ingredient => 
-				ingredient.ingredient.toLowerCase().includes(searchValue)
-			);
-			return recipeName.includes(searchValue) || recipeDescription.includes(searchValue) || ingredientMatch;
-		});
-	}
+	if(searchValue.length === 0)
+		currentRecipes = [...recipes];
 
 	if (tags.size > 0) {
-
 		currentRecipes = currentRecipes.filter(recipe => {
 			return Array.from(tags).every(tag => {
 				const tagLower = tag.toLowerCase();
@@ -105,80 +49,13 @@ const updateWithTags = () => {
 			});
 		});
 	}
-	console.log("currentRecipes: ", currentRecipes, "tags: ", tags);
 	displayRecipes();
 	updateListDisplay();
-    listSearchDis();
+    listSearchSort();
 };
 
-    const createTag = (text) => {
-	const tagContainer = document.getElementById('tag-container');
 
-		if(tags.has(text)) return;
-        const tag = document.createElement('span');
-        tag.className = 'bg-yellow-400 py-1 px-3 rounded-xl text-sm flex items-center gap-2 m-1';
-        tag.innerHTML = `
-            ${text}
-            <i class="fa-solid fa-x cursor-pointer delete-tag"></i>`;
-        
-		const deleteTag = tag.querySelector('.delete-tag');
-		deleteTag.addEventListener('click', () => {
-			tag.remove();
-            tags.delete(text);
-			updateWithTags();
-		})
-        tagContainer.appendChild(tag);
-        tags.add(text);
-		updateWithTags();
-    };
-
-
-
-const listSearch = (type) => {
-	console.log("LALALLALSLSLSLSLSLL")
-	const search = document.getElementById(`${type}-search`);
-	const list = document.getElementById(`${type}-ul`);
-	const items = list.getElementsByTagName('li');
-	let searchValue = "";
-	let searchValueLower = "";
-	let filteredItems = [];
-
-	Array.from(items).forEach(item => {
-        item.addEventListener('click', () => {
-			console.log("CIICICICI")
-            createTag(item.textContent);
-            
-            const list = document.querySelector(`.${type}-list`);
-            const toggle = document.querySelector(`.toggle-${type}`);
-            if (list && toggle) {
-                list.classList.add('hidden');
-                toggle.classList.remove('hidden');
-            }
-        });
-    });
-
-	search.addEventListener('input', (e) => {
-		searchValue = e.target.value;
-
-		if(searchValue.length > 2){
-			searchValueLower = searchValue.toLowerCase();
-			filteredItems = Array.from(items).filter(item => {
-				return item.textContent.toLowerCase().includes(searchValueLower);
-			})
-		}else{
-			filteredItems = Array.from(items);
-		}
-
-		Array.from(items).forEach(item => {
-			item.classList.add('hidden');
-		})
-
-		filteredItems.forEach(item => {
-			item.classList.remove('hidden');
-		})
-	})
-}
-
+//display recipes cards
 const displayRecipes = () => {
 	const wrapperCard = document.getElementById('wrapper-card');
     wrapperCard.innerHTML = "";
@@ -187,9 +64,7 @@ const displayRecipes = () => {
 		const card = cardTemplates(recipe);
 			wrapperCard.appendChild(card);
     });
-	
 }
-
 
 const  updateListDisplay = () => {
 	updateList(currentRecipes, 'ingredient');
@@ -197,7 +72,7 @@ const  updateListDisplay = () => {
 	updateList(currentRecipes, 'ustensils');
 }
 
-const listSearchDis = () => {
+const listSearchSort = () => {
 	listSearch('ingredient');
 	listSearch('devices');
 	listSearch('ustensils');
@@ -211,11 +86,8 @@ const init = async () => {
 	listIngredient.appendChild(listTemplates(recipes, 'ingredient'));
 	listDevices.appendChild(listTemplates(recipes, 'devices'));
 	listUstensils.appendChild(listTemplates(recipes, 'ustensils'));
-	listSearchDis();
+	listSearchSort();
 	displayRecipes();
 }
-
-console.log("current: ", currentRecipes)
-
 
 init();
---
+++ scripts/utils/list.js
@@ -0,0 +1,118 @@
+import { tags } from "../../index.js";
+import { updateWithTags } from "../../index.js";
+
+
+const toggleIngredient = () => {
+	const list = document.querySelector('.ingredient-list');
+	const toggleDevice = document.querySelector('.toggle-ingredient');
+	const btnListe = document.querySelector('.btn-ingredient-list');
+
+	list.classList.toggle('hidden');
+	toggleDevice.classList.toggle('hidden');
+
+	btnListe.addEventListener('click', () => {
+		list.classList.add('hidden');
+		toggleDevice.classList.remove('hidden');
+	})
+}
+window.toggleIngredient = toggleIngredient;
+
+const toggleDevices = () => {
+	const list = document.querySelector('.devices-list');
+	const toggleDevice = document.querySelector('.toggle-devices');
+	const btnListe = document.querySelector('.btn-devices-list');
+
+	list.classList.toggle('hidden');
+	toggleDevice.classList.toggle('hidden');
+
+	btnListe.addEventListener('click', () => {
+		list.classList.add('hidden');
+		toggleDevice.classList.remove('hidden');
+	})
+	
+}
+window.toggleDevices = toggleDevices;
+
+
+const toggleUstensils = () => {
+	const list = document.querySelector('.ustensils-list');
+	const toggleDevice = document.querySelector('.toggle-ustensils');
+	const btnListe = document.querySelector('.btn-ustensils-list');
+
+	list.classList.toggle('hidden');
+	toggleDevice.classList.toggle('hidden');
+
+	btnListe.addEventListener('click', () => {
+		list.classList.add('hidden');
+		toggleDevice.classList.remove('hidden');
+	})
+	
+}
+window.toggleUstensils = toggleUstensils;
+
+
+export const listSearch = (type) => {
+	const search = document.getElementById(`${type}-search`);
+	const list = document.getElementById(`${type}-ul`);
+	const items = list.getElementsByTagName('li');
+	let searchValue = "";
+	let searchValueLower = "";
+	let filteredItems = [];
+
+	//creation of tags
+	Array.from(items).forEach(item => {
+        item.addEventListener('click', () => {
+            createTag(item.textContent);
+            
+            const list = document.querySelector(`.${type}-list`);
+            const toggle = document.querySelector(`.toggle-${type}`);
+            if (list && toggle) {
+                list.classList.add('hidden');
+                toggle.classList.remove('hidden');
+            }
+        });
+    });
+
+	//sort list
+	search.addEventListener('input', (e) => {
+		searchValue = e.target.value;
+
+		if(searchValue.length > 1){
+			searchValueLower = searchValue.toLowerCase();
+			filteredItems = Array.from(items).filter(item => {
+				return item.textContent.toLowerCase().includes(searchValueLower);
+			})
+		}else{
+			filteredItems = Array.from(items);
+		}
+
+		Array.from(items).forEach(item => {
+			item.classList.add('hidden');
+		})
+
+		filteredItems.forEach(item => {
+			item.classList.remove('hidden');
+		})
+	})
+}
+
+const createTag = (text) => {
+	const tagContainer = document.getElementById('tag-container');
+
+	if(tags.has(text)) return;
+	const tag = document.createElement('span');
+	tag.className = 'bg-yellow-400 py-1 px-3 rounded-xl text-sm flex items-center gap-2 m-1';
+	tag.innerHTML = `
+		${text}
+		<i class="fa-solid fa-x cursor-pointer delete-tag"></i>`;
+	
+	const deleteTag = tag.querySelector('.delete-tag');
+	deleteTag.addEventListener('click', () => {
+		tag.remove();
+		tags.delete(text);
+		updateWithTags();
+	})
+	tagContainer.appendChild(tag);
+	tags.add(text);
+	updateWithTags();
+};
